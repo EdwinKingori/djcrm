aa2fc5f1dadeee4f5b707e812914bccdba49a1a0
EDDY <eddmunyiri@gmail.com>
2024-01-11 22:12:39 +0300

    Detail view

--- leads/views.py
+++ leads/views.py
@@ -9,3 +9,11 @@ def index(request):
         "leads": leads
     }
     return render(request, "leads/index.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_detail.html", context)
