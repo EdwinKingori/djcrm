3dee357646b4437d02b273aa44773e57154b60a2
EDDY <eddmunyiri@gmail.com>
2024-01-17 22:06:09 +0300

    adding authentication template

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -14,16 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from leads.views import LandingPageView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth.views import LoginView
+from django.urls import path, include
+from leads.views import LandingPageView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name="landing-page"),
     path('leads/', include('leads.urls')),
+    path('login/', LoginView.as_view(), name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
