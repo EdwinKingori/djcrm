7c03a7174d7d74f2cf36755085cd2c070628842e
EDDY <eddmunyiri@gmail.com>
2024-01-19 20:46:23 +0300

    Tests

--- leads/tests.py
+++ leads/tests.py
@@ -6,6 +6,16 @@ from django.shortcuts import reverse
 
 class LandingPageTest(TestCase):
 
-    def test_status_code(self):
+    # def test_status_code(self):
+    #     response = self.client.get(reverse("landing-page"))
+    #     self.assertEqual(response.status_code, 200)
+
+    # def test_template_name(self):
+    #     response = self.client.get(reverse("landing-page"))
+    #     self.assertTemplateUsed(response, "landing.html")
+
+    # shortening the test code above
+    def test_get(self):
         response = self.client.get(reverse("landing-page"))
-        print(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "landing.html")
