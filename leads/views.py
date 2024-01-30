71aca14ad4bf6168db7eea2441cca8822475e5e5
EDDY <eddmunyiri@gmail.com>
2024-01-30 23:28:56 +0300

    updating  category view and template

--- leads/views.py
+++ leads/views.py
@@ -223,10 +223,28 @@ class CategoryListView(LoginRequiredMixin, ListView):
     template_name = "leads/category_list.html"
     context_object_name = "category_list"
 
-    def get_queryset(self):
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
         user = self.request.user
 
         # initial queryset  of leads for the entire organization
+        if user.is_organizer:
+            # if the user is an organizer, then they have a user profile(acessed through user.userprofile in the database)
+            queryset = Lead.objects.filter(
+                organization=user.userprofile)
+        else:
+            # else if they are not an organizer, being an agent, we filter the agent through the current organization
+            queryset = Lead.objects.filter(
+                organization=user.agent.organization)
+
+        context.update({
+            "unassigned_lead_count": queryset.filter(category__isnull=True).count()
+        })
+        return context
+
+    def get_queryset(self):
+        user = self.request.user
+
         if user.is_organizer:
             # if the user is an organizer, then they have a user profile(acessed through user.userprofile in the database)
             queryset = Category.objects.filter(
@@ -236,7 +254,6 @@ class CategoryListView(LoginRequiredMixin, ListView):
             queryset = Category.objects.filter(
                 organization=user.agent.organization)
         return queryset
-
 
 # Manual update function
 # def update_lead(request, pk):
